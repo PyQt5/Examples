4f5e877b2812533164307d64a8c03814d8424af2
Irony <892768447@qq.com>
2020-06-27 23:04:21 +0800

    5.15.0

--- PySide2/widgets/richtext/syntaxhighlighter/syntaxhighlighter_rc.py
+++ PySide2/widgets/richtext/syntaxhighlighter/syntaxhighlighter_rc.py
@@ -1,117 +1,122 @@
 # Resource object code (Python 3)
 # Created by: object code
-# Created by: The Resource Compiler for Qt version 5.14.0
+# Created by: The Resource Compiler for Qt version 5.15.0
 # WARNING! All changes made in this file will be lost!
 
 from PySide2 import QtCore
 
 qt_resource_data = b"\
-\x00\x00\x06{\
+\x00\x00\x06\xca\
 T\
-EMPLATE = app\x0aLA\
-NGUAGE = C++\x0aTAR\
-GET         = as\
-sistant\x0a\x0aCONFIG \
-       += qt war\
-n_on\x0aQT         \
-   += xml networ\
-k\x0a\x0aPROJECTNAME  \
-      = Assistan\
-t\x0aDESTDIR       \
-     = ../../bin\
-\x0a\x0aFORMS += findd\
-ialog.ui \x5c\x0a     \
-   helpdialog.ui\
- \x5c\x0a        mainw\
-indow.ui \x5c\x0a     \
-   settingsdialo\
-g.ui \x5c\x0a        t\
-abbedbrowser.ui \
-\x5c\x0a        topicc\
-hooser.ui\x0a\x0aSOURC\
-ES += main.cpp \x5c\
-\x0a        helpwin\
-dow.cpp \x5c\x0a      \
-  topicchooser.c\
-pp \x5c\x0a        doc\
-uparser.cpp \x5c\x0a  \
-      settingsdi\
-alog.cpp \x5c\x0a     \
-   index.cpp \x5c\x0a \
-       profile.c\
-pp \x5c\x0a        con\
-fig.cpp \x5c\x0a      \
-  finddialog.cpp\
- \x5c\x0a        helpd\
-ialog.cpp \x5c\x0a    \
-    mainwindow.c\
-pp \x5c\x0a        tab\
-bedbrowser.cpp\x0a\x0a\
-HEADERS        +\
-= helpwindow.h \x5c\
+EMPLATE = app\x0d\x0aL\
+ANGUAGE = C++\x0d\x0aT\
+ARGET         = \
+assistant\x0d\x0a\x0d\x0aCON\
+FIG        += qt\
+ warn_on\x0d\x0aQT    \
+        += xml n\
+etwork\x0d\x0a\x0d\x0aPROJEC\
+TNAME        = A\
+ssistant\x0d\x0aDESTDI\
+R            = .\
+./../bin\x0d\x0a\x0d\x0aFORM\
+S += finddialog.\
+ui \x5c\x0d\x0a        he\
+lpdialog.ui \x5c\x0d\x0a \
+       mainwindo\
+w.ui \x5c\x0d\x0a        \
+settingsdialog.u\
+i \x5c\x0d\x0a        tab\
+bedbrowser.ui \x5c\x0d\
 \x0a        topicch\
-ooser.h \x5c\x0a      \
+ooser.ui\x0d\x0a\x0d\x0aSOUR\
+CES += main.cpp \
+\x5c\x0d\x0a        helpw\
+indow.cpp \x5c\x0d\x0a   \
+     topicchoose\
+r.cpp \x5c\x0d\x0a       \
+ docuparser.cpp \
+\x5c\x0d\x0a        setti\
+ngsdialog.cpp \x5c\x0d\
+\x0a        index.c\
+pp \x5c\x0d\x0a        pr\
+ofile.cpp \x5c\x0d\x0a   \
+     config.cpp \
+\x5c\x0d\x0a        findd\
+ialog.cpp \x5c\x0d\x0a   \
+     helpdialog.\
+cpp \x5c\x0d\x0a        m\
+ainwindow.cpp \x5c\x0d\
+\x0a        tabbedb\
+rowser.cpp\x0d\x0a\x0d\x0aHE\
+ADERS        += \
+helpwindow.h \x5c\x0d\x0a\
+        topiccho\
+oser.h \x5c\x0d\x0a      \
   docuparser.h \x5c\
-\x0a        setting\
-sdialog.h \x5c\x0a    \
-    index.h \x5c\x0a  \
-      profile.h \
-\x5c\x0a        finddi\
-alog.h \x5c\x0a       \
- helpdialog.h \x5c\x0a\
-        mainwind\
-ow.h \x5c\x0a        t\
-abbedbrowser.h \x5c\
-\x0a        config.\
-h\x0a\x0aRESOURCES += \
-assistant.qrc\x0a\x0aD\
-EFINES += QT_KEY\
-WORDS\x0a#DEFINES +\
-=  QT_PALMTOPCEN\
-TER_DOCS\x0a!networ\
-k:DEFINES       \
- += QT_INTERNAL_\
-NETWORK\x0aelse:QT \
-+= network\x0a!xml:\
+\x0d\x0a        settin\
+gsdialog.h \x5c\x0d\x0a  \
+      index.h \x5c\x0d\
+\x0a        profile\
+.h \x5c\x0d\x0a        fi\
+nddialog.h \x5c\x0d\x0a  \
+      helpdialog\
+.h \x5c\x0d\x0a        ma\
+inwindow.h \x5c\x0d\x0a  \
+      tabbedbrow\
+ser.h \x5c\x0d\x0a       \
+ config.h\x0d\x0a\x0d\x0aRES\
+OURCES += assist\
+ant.qrc\x0d\x0a\x0d\x0aDEFIN\
+ES += QT_KEYWORD\
+S\x0d\x0a#DEFINES +=  \
+QT_PALMTOPCENTER\
+_DOCS\x0d\x0a!network:\
+DEFINES        +\
+= QT_INTERNAL_NE\
+TWORK\x0d\x0aelse:QT +\
+= network\x0d\x0a!xml:\
  DEFINES        \
         += QT_IN\
-TERNAL_XML\x0aelse:\
-QT += xml\x0ainclud\
-e( ../../src/qt_\
-professional.pri\
- )\x0a\x0awin32 {\x0a    \
-LIBS += -lshell3\
-2\x0a    RC_FILE = \
-assistant.rc\x0a}\x0a\x0a\
-macos {\x0a    ICON\
- = assistant.icn\
-s\x0a    TARGET = a\
-ssistant\x0a#    QM\
-AKE_INFO_PLIST =\
- Info_mac.plist\x0a\
-}\x0a\x0a#target.path \
-= $$[QT_INSTALL_\
-BINS]\x0a#INSTALLS \
-+= target\x0a\x0a#assi\
-stanttranslation\
-s.files = *.qm\x0a#\
-assistanttransla\
-tions.path = $$[\
-QT_INSTALL_TRANS\
-LATIONS]\x0a#INSTAL\
-LS += assistantt\
-ranslations\x0a\x0aTRA\
-NSLATIONS       \
- = assistant_de.\
-ts \x5c\x0a           \
-       assistant\
-_fr.ts\x0a\x0a\x0aunix:!c\
-ontains(QT_CONFI\
-G, zlib):LIBS +=\
- -lz\x0a\x0a\x0atarget.pa\
-th=$$[QT_INSTALL\
-_BINS]\x0aINSTALLS \
-+= target\x0a\
+TERNAL_XML\x0d\x0aelse\
+:QT += xml\x0d\x0aincl\
+ude( ../../src/q\
+t_professional.p\
+ri )\x0d\x0a\x0d\x0awin32 {\x0d\
+\x0a    LIBS += -ls\
+hell32\x0d\x0a    RC_F\
+ILE = assistant.\
+rc\x0d\x0a}\x0d\x0a\x0d\x0amacos {\
+\x0d\x0a    ICON = ass\
+istant.icns\x0d\x0a   \
+ TARGET = assist\
+ant\x0d\x0a#    QMAKE_\
+INFO_PLIST = Inf\
+o_mac.plist\x0d\x0a}\x0d\x0a\
+\x0d\x0a#target.path =\
+ $$[QT_INSTALL_B\
+INS]\x0d\x0a#INSTALLS \
++= target\x0d\x0a\x0d\x0a#as\
+sistanttranslati\
+ons.files = *.qm\
+\x0d\x0a#assistanttran\
+slations.path = \
+$$[QT_INSTALL_TR\
+ANSLATIONS]\x0d\x0a#IN\
+STALLS += assist\
+anttranslations\x0d\
+\x0a\x0d\x0aTRANSLATIONS \
+       = assista\
+nt_de.ts \x5c\x0d\x0a    \
+              as\
+sistant_fr.ts\x0d\x0a\x0d\
+\x0a\x0d\x0aunix:!contain\
+s(QT_CONFIG, zli\
+b):LIBS += -lz\x0d\x0a\
+\x0d\x0a\x0d\x0atarget.path=\
+$$[QT_INSTALL_BI\
+NS]\x0d\x0aINSTALLS +=\
+ target\x0d\x0a\
 "
 
 qt_resource_name = b"\
@@ -131,7 +136,7 @@ qt_resource_struct = b"\
 \x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x02\
 \x00\x00\x00\x00\x00\x00\x00\x00\
 \x00\x00\x00\x16\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\
-\x00\x00\x01e\xaf\x16\xd2\xa1\
+\x00\x00\x01rR\x22\xae\x00\
 "
 
 def qInitResources():
--- PySide2/xmlpatterns/schema/schema_rc.py
+++ PySide2/xmlpatterns/schema/schema_rc.py
@@ -1,326 +1,292 @@
 # Resource object code (Python 3)
 # Created by: object code
-# Created by: The Resource Compiler for Qt version 5.14.0
+# Created by: The Resource Compiler for Qt version 5.15.0
 # WARNING! All changes made in this file will be lost!
 
 from PySide2 import QtCore
 
 qt_resource_data = b"\
-\x00\x00\x01\x81\
+\x00\x00\x01\x84\
 \x00\
-\x00\x06\x05x\x9c\xbdT\xc1r\xc2 \x10\xbd\xfb\x15\x0c\
-\x1f`\xb4\xbd9F\xcf\x9di\xa7\x87\xf6\xd0+\x92\x1d\
-\xc3L\x80\x08\x8b\x89\x7f_\x82\x1am\x0cI\xbc\x94C\
-&\xc3\xbe\xf7\xd8]\x1e\xbb\xde\xd6\xb2 G0Vh\
-\x95\xd2\xe5|A\xb7\x9b\xd9\xba\xb6\xd9\xca\xf2\x1c$#\
->\xae\xec\xcao\xa44G,WIRU\xd5\xbcz\
-\x9dk\xb3O^\x16\x8be\xf2\xf3\xf1\xfe\x15\xb0t3\
-\x9b\x11\xbf\x02\x1d\x0a\x90\xa0\x90(&!\xa5\x06\xb8(\
-\xc1\x03\xc8e\x05\x0c\xd7\xb2,\xa0\xfe>\x95p\x8b\xb4\
-Q\x0b\x07\x07\x8awB\x91\x03P`\x01\x94\xa0\x97J\
-i`\xa3\x11jO\x93Il\x8f4\x90\x09\xbfq\x95\
-\xb8\xed4\xd9Q\x22Y\xfd\xc9\xb936\xa5N\xed\xb4\
-S\x19d\x13\xc5Q\xc86\xb3\xe6?\x08N\xa3J\xc0\
-\x5cg\xf4\x11;\xde\xc2'\xda9\x92\x84E({:\
-;\xb9%\xadt2\x9e\xc6\x193X\xd4\x19rI\xb1\
-c\x9b\xc8\x09\x11\xd5\x8e\xd2\xcd\xbbw\xc8\x07\x83\x84\xdb\
-\xeb\xf8\x98\xa1o\xc9\xce\xe1\x15\xdd|\x87\xbd\xd8K;\
-8\xa6\xbc\x8fO\xf7\xd4R[\x81\xe2\x08o\x0aa\x0f\
-fT\xc3)\x81\xf1\xa3{\x1a1Tu\xeb\xd6\xffJ\
-\xbco\x0a\x88&\xab\x88\x15\x1a\x80\x81\xa6F\x8e~\x80\
-\x91\x1d\xb3\x7f\xeb\x1ex9\xa0\x9c\x04\xc3\x02\xef\xc8\x0a\
-\xd78\x1d\xb8V\x99\x8d\x998F\x93B\xf9:\x9e\xa6\
-\xe5\xda\xbf\x9d\xdeA\x90t\xea\xeauyO_\xce\x91\
-\xb6\xb3\xf1;\xbfH\x84\xa9\xbd\x99\xfd\x02,\xd8\xc0\x1d\
-\
-\x00\x00\x02U\
+\x00\x06-x\x9c\xbdT\xb1n\xc20\x10\xdd+\xf1\x0f\
+\x96?\x80@\xbb!R\xe6J\xad:\xb4CW\xe3\x9c\
+\xc0Rl\x07\xfbL\xc2\xdf\xd76\x90\xa6\xa9\x13\xc2R\
+\x0fQt\xf7\xde\xf3\xdd\xe5\xe5\xd6\x9bF\x96\xe4\x08\xc6\
+\x0a\xadr\xba\x9c/\xe8\xe6y\xf6\xb0nl\xb1\xb2|\
+\x0f\x92\x11\x0fPv\xe5\x039\xdd#V\xab,\xab\xeb\
+z^?\xcd\xb5\xd9e\x8f\x8b\xc52\xfbz{\xfd\x88\
+X\xea\xa9\xb3\x07\xe2O\x14\x80\x12$($\x8aI\xc8\
+\xa9\x01.*\x08\x10r9\x11\xc4\xb5\xacJh>O\
+\x15tRm\xda\xc2\xc1\x81\xe2\xfd\xdc\xc0\x1d(\xb0\x04\
+J\xd0\x8b\xe54\xd2\xd1\x08\xb5\xa3\xd94\xba\x87\x1a(\
+\x84\x0f\x5c5~\x22\xa1@J$k\xde9w\xc6\xe6\
+\xd4\xa9\xadv\xaa\x80b\xaa:\x0a\xd9\xd6\x16\xde\xa3\xe2\
+D\xae\x04\xdc\xeb\x82&\xc0\x13\x06y\xcfPo\xd4a\
+\x11\xaa\xc4|\xa7\xcf\xa5\xd5\xce&\x14r\x06\x8d7v\
+\xc6\x5c\xaa\xec;h\xe8\x92!\xe1\xbeX\xc7\xcc\x1d\xf0\
+\x1f\xb7\xc4/\xd976C?\x9b\xad\xc3+<<o\
+X3\xc9;8\xa6\xbc\xafO]n\xa5\xad@q\x84\
+\x17\x85\xb0\x03s[\xc4)\x81#\x97\xa7\xe61\xda{\
+\xeb\xdf\xff\xab>\xb9\x1dD(l\xc8\x19\x01a \xb4\
+\xca\xd1\xaf7\xb2e\xf6w\xfbc\xbf\x13('\xc1\xb0\
+H<\xb2\xd2\x05\xf3\x03\xd7\xaa\xb0\x83\xbe\x1e\xe2I\xa1\
+|/\xf7\xf3\xf6\xda\xffP\xe9\x0d\x91\xf5zK\x1b?\
+5\x9ds\xaa\x9d\xf0\xa8\x01.2q\xb7\xfb\xc07\x9f\
+\x0c\xc2%\
+\x00\x00\x02c\
 <\
-recipe>\x0a    <tit\
-le>Cheese on Toa\
-st</title>\x0a    <\
-ingredient name=\
-\x22Bread\x22 quantity\
-=\x222\x22 unit=\x22slice\
-s\x22/>\x0a    <ingred\
-ient name=\x22Chees\
-e\x22 quantity=\x222\x22 \
-unit=\x22slices\x22/>\x0a\
-    <time quanti\
-ty=\x223\x22 unit=\x22day\
-s\x22/>\x0a    <method\
->\x0a        <step>\
-1. Slice the bre\
-ad and cheese.</\
-step>\x0a        <s\
-tep>2. Grill one\
- side of each sl\
-ice of bread.</s\
-tep>\x0a        <st\
-ep>3. Turn over \
-the bread and pl\
-ace a slice of c\
-heese on each pi\
-ece.</step>\x0a    \
-    <step>4. Gri\
-ll until the che\
-ese has started \
-to melt.</step>\x0a\
-        <step>5.\
- Serve and enjoy\
-!</step>\x0a    </m\
-ethod>\x0a    <comm\
-ent>Tell your fr\
-iends about it!<\
-/comment>\x0a</reci\
-pe>\x0a\
-\x00\x00\x03\xbb\
+recipe>\x0d\x0a    <ti\
+tle>Cheese on To\
+ast</title>\x0d\x0a   \
+ <ingredient nam\
+e=\x22Bread\x22 quanti\
+ty=\x222\x22 unit=\x22sli\
+ces\x22/>\x0d\x0a    <ing\
+redient name=\x22Ch\
+eese\x22 quantity=\x22\
+2\x22 unit=\x22slices\x22\
+/>\x0d\x0a    <time qu\
+antity=\x223\x22 unit=\
+\x22days\x22/>\x0d\x0a    <m\
+ethod>\x0d\x0a        \
+<step>1. Slice t\
+he bread and che\
+ese.</step>\x0d\x0a   \
+     <step>2. Gr\
+ill one side of \
+each slice of br\
+ead.</step>\x0d\x0a   \
+     <step>3. Tu\
+rn over the brea\
+d and place a sl\
+ice of cheese on\
+ each piece.</st\
+ep>\x0d\x0a        <st\
+ep>4. Grill unti\
+l the cheese has\
+ started to melt\
+.</step>\x0d\x0a      \
+  <step>5. Serve\
+ and enjoy!</ste\
+p>\x0d\x0a    </method\
+>\x0d\x0a    <comment>\
+Tell your friend\
+s about it!</com\
+ment>\x0d\x0a</recipe>\
+\x0d\x0a\
+\x00\x00\x01$\
+\x00\
+\x00\x03\xd4x\x9c\xa5\x93\xcfR\x830\x10\xc6\xef\x9d\xe9\
+;d\xf2\x00\x84\xea\x8d\x01;\x8eW\xff\x1c\xf4\xe05\
+\x86\x152\x92\x04\x93\xa5\x80Oo\x04\xaam\x0d\xcah\
+Nd\xf7\xdb_\x96|\x9bt\xdb\xa9\x8a\xec\xc0:i\
+tF7QL\xb7\x17\xebU\xda\xb9<q\xa2\x04\xc5\
+\x89\x17h\x97\xf8@FK\xc4:a\xacm\xdb\xa8=\
+\x8f\x8c-\xd8Y\x1co\xd8\xe3\xcd\xf5\xfd\xa0\xa5\xbet\
+\xbd\x22~\x0d\x00\xa8@\x81F\xa2\xb9\x82\x8c\x0a\xa3\x91\
+\x0b\xfc\xd0\x90i\x0d*aT]A\xf7\xd0\xd7p\x90\
+\xfaL;xm@\x8b\xd3\xdc\xcc!\x85\xdc\x81\xbe\xf5\
+\x9f\x94\xa0\x07ft@\xa0\x95\xba\xa0l\x19\xe2\x99+\
+Y\xf5\xffc<I\x8be\xce\xf1\x081\xee\x95\xd4w\
+B4\xd6e4^\x8a+\x8d\x82\xcb<\xb7\xe0\xdc\x1e\
+\xc8\xa7\xedBBk\xec\xcb\x0c\xe1\xe7\x8eR6c\xc2\
+\x98\xf8n\xde\x18\x9fN?\x9e\x87\x03\xf1\xd4\xd6\xbe\x87\
+\xd3\x99\x981=\xf0c\xde\x17\x00\x5c\xe0T\xa0\xf6M\
+\xd6W&_bsh\x9c%\xf6\x7f\xac4\x8dF\xfb\
+[q\xf0\xe2C\x97\xee_+\xfbz\xae>\xf0\x0e\x08\
+c\x1f\xb0\
+\x00\x00\x022\
+<\
+recipe>\x0d\x0a    <ti\
+tle>Cheese on To\
+ast</title>\x0d\x0a   \
+ <ingredient nam\
+e=\x22Bread\x22 quanti\
+ty=\x222\x22 unit=\x22sli\
+ces\x22/>\x0d\x0a    <ing\
+redient name=\x22Ch\
+eese\x22 quantity=\x22\
+2\x22 unit=\x22slices\x22\
+/>\x0d\x0a    <time qu\
+antity=\x223\x22 unit=\
+\x22minutes\x22/>\x0d\x0a   \
+ <method>\x0d\x0a     \
+   <step>1. Slic\
+e the bread and \
+cheese.</step>\x0d\x0a\
+        <step>2.\
+ Grill one side \
+of each slice of\
+ bread.</step>\x0d\x0a\
+        <step>3.\
+ Turn over the b\
+read and place a\
+ slice of cheese\
+ on each piece.<\
+/step>\x0d\x0a        \
+<step>4. Grill u\
+ntil the cheese \
+has started to m\
+elt.</step>\x0d\x0a   \
+     <step>5. Se\
+rve and enjoy!</\
+step>\x0d\x0a    </met\
+hod>\x0d\x0a</recipe>\x0d\
+\x0a\
+\x00\x00\x01(\
+<\
+contact>\x0d\x0a    <g\
+ivenName>John</g\
+ivenName>\x0d\x0a    <\
+familyName>Doe</\
+familyName>\x0d\x0a   \
+ <title>Prof.</t\
+itle>\x0d\x0a    <work\
+Address>\x0d\x0a      \
+  <street>Sandak\
+erveien 116</str\
+eet>\x0d\x0a        <z\
+ipCode>N-0550</z\
+ipCode>\x0d\x0a       \
+ <city>Oslo</cit\
+y>\x0d\x0a        <cou\
+ntry>Norway</cou\
+ntry>\x0d\x0a    </wor\
+kAddress>\x0d\x0a</con\
+tact>\x0d\x0a\
+\x00\x00\x01;\
+<\
+order>\x0d\x0a    <cus\
+tomerId>234219</\
+customerId>\x0d\x0a   \
+ <article>\x0d\x0a    \
+    <articleId>2\
+1692</articleId>\
+\x0d\x0a        <count\
+>3</count>\x0d\x0a    \
+</article>\x0d\x0a    \
+<article>\x0d\x0a     \
+   <articleId>24\
+749</articleId>\x0d\
+\x0a        <count>\
+9</count>\x0d\x0a    <\
+/article>\x0d\x0a    <\
+deliveryDate>200\
+9-01-23</deliver\
+yDate>\x0d\x0a    <pay\
+ed>yes</payed>\x0d\x0a\
+</order>\x0d\x0a\
+\x00\x00\x015\
+<\
+contact>\x0d\x0a    <g\
+ivenName>John</g\
+ivenName>\x0d\x0a    <\
+familyName>Doe</\
+familyName>\x0d\x0a   \
+ <birthdate>1977\
+-12-25</birthdat\
+e>\x0d\x0a    <homeAdd\
+ress>\x0d\x0a        <\
+street>Sandakerv\
+eien 116</street\
+>\x0d\x0a        <zipC\
+ode>N-0550</zipC\
+ode>\x0d\x0a        <c\
+ity>Oslo</city>\x0d\
+\x0a        <countr\
+y>Norway</countr\
+y>\x0d\x0a    </homeAd\
+dress>\x0d\x0a</contac\
+t>\x0d\x0a\
+\x00\x00\x03~\
 <\
 ?xml version=\x221.\
-0\x22?>\x0a<xsd:schema\
- xmlns:xsd=\x22http\
-://www.w3.org/20\
-01/XMLSchema\x22>\x0a\x0a\
-    <xsd:element\
- name=\x22contact\x22>\
-\x0a        <xsd:co\
-mplexType>\x0a     \
-       <xsd:sequ\
-ence>\x0a          \
-      <xsd:eleme\
-nt name=\x22givenNa\
-me\x22 type=\x22xsd:st\
-ring\x22/>\x0a        \
-        <xsd:ele\
-ment name=\x22famil\
-yName\x22 type=\x22xsd\
-:string\x22/>\x0a     \
-           <xsd:\
-element name=\x22bi\
-rthdate\x22 type=\x22x\
-sd:date\x22 minOccu\
-rs=\x220\x22/>\x0a       \
-         <xsd:el\
-ement name=\x22home\
-Address\x22 type=\x22a\
-ddress\x22/>\x0a      \
+0\x22?>\x0d\x0a<xsd:schem\
+a xmlns:xsd=\x22htt\
+p://www.w3.org/2\
+001/XMLSchema\x22>\x0d\
+\x0a\x0d\x0a    <xsd:elem\
+ent name=\x22order\x22\
+>\x0d\x0a        <xsd:\
+complexType>\x0d\x0a  \
+          <xsd:s\
+equence>\x0d\x0a      \
           <xsd:e\
-lement name=\x22wor\
-kAddress\x22 type=\x22\
-address\x22 minOccu\
-rs=\x220\x22/>\x0a       \
-     </xsd:seque\
-nce>\x0a        </x\
-sd:complexType>\x0a\
-    </xsd:elemen\
-t>\x0a\x0a    <xsd:com\
-plexType name=\x22a\
-ddress\x22>\x0a       \
- <xsd:sequence>\x0a\
-            <xsd\
-:element name=\x22s\
-treet\x22 type=\x22xsd\
-:string\x22/>\x0a     \
+lement name=\x22cus\
+tomerId\x22 type=\x22x\
+sd:positiveInteg\
+er\x22/>\x0d\x0a         \
        <xsd:elem\
-ent name=\x22zipCod\
-e\x22 type=\x22xsd:str\
-ing\x22/>\x0a         \
+ent name=\x22articl\
+e\x22 type=\x22article\
+Type\x22 maxOccurs=\
+\x22unbounded\x22/>\x0d\x0a \
+               <\
+xsd:element name\
+=\x22deliveryDate\x22 \
+type=\x22xsd:date\x22/\
+>\x0d\x0a             \
    <xsd:element \
-name=\x22city\x22 type\
-=\x22xsd:string\x22/>\x0a\
-            <xsd\
-:element name=\x22c\
-ountry\x22 type=\x22xs\
-d:string\x22/>\x0a    \
-    </xsd:sequen\
-ce>\x0a    </xsd:co\
-mplexType>\x0a\x0a</xs\
-d:schema>\x0a\
-\x00\x00\x02%\
-<\
-recipe>\x0a    <tit\
-le>Cheese on Toa\
-st</title>\x0a    <\
-ingredient name=\
-\x22Bread\x22 quantity\
-=\x222\x22 unit=\x22slice\
-s\x22/>\x0a    <ingred\
-ient name=\x22Chees\
-e\x22 quantity=\x222\x22 \
-unit=\x22slices\x22/>\x0a\
-    <time quanti\
-ty=\x223\x22 unit=\x22min\
-utes\x22/>\x0a    <met\
-hod>\x0a        <st\
-ep>1. Slice the \
-bread and cheese\
-.</step>\x0a       \
- <step>2. Grill \
-one side of each\
- slice of bread.\
-</step>\x0a        \
-<step>3. Turn ov\
-er the bread and\
- place a slice o\
-f cheese on each\
- piece.</step>\x0a \
-       <step>4. \
-Grill until the \
-cheese has start\
-ed to melt.</ste\
-p>\x0a        <step\
->5. Serve and en\
-joy!</step>\x0a    \
-</method>\x0a</reci\
-pe>\x0a\
-\x00\x00\x01\x1d\
-<\
-contact>\x0a    <gi\
-venName>John</gi\
-venName>\x0a    <fa\
-milyName>Doe</fa\
-milyName>\x0a    <t\
-itle>Prof.</titl\
-e>\x0a    <workAddr\
-ess>\x0a        <st\
-reet>Sandakervei\
-en 116</street>\x0a\
-        <zipCode\
->N-0550</zipCode\
->\x0a        <city>\
-Oslo</city>\x0a    \
-    <country>Nor\
-way</country>\x0a  \
-  </workAddress>\
-\x0a</contact>\x0a\
-\x00\x00\x01.\
-<\
-order>\x0a    <cust\
-omerId>234219</c\
-ustomerId>\x0a    <\
-article>\x0a       \
- <articleId>2169\
-2</articleId>\x0a  \
-      <count>3</\
-count>\x0a    </art\
-icle>\x0a    <artic\
-le>\x0a        <art\
-icleId>24749</ar\
-ticleId>\x0a       \
- <count>9</count\
->\x0a    </article>\
-\x0a    <deliveryDa\
-te>2009-01-23</d\
-eliveryDate>\x0a   \
- <payed>yes</pay\
-ed>\x0a</order>\x0a\
-\x00\x00\x01*\
-<\
-contact>\x0a    <gi\
-venName>John</gi\
-venName>\x0a    <fa\
-milyName>Doe</fa\
-milyName>\x0a    <b\
-irthdate>1977-12\
--25</birthdate>\x0a\
-    <homeAddress\
->\x0a        <stree\
-t>Sandakerveien \
-116</street>\x0a   \
-     <zipCode>N-\
-0550</zipCode>\x0a \
-       <city>Osl\
-o</city>\x0a       \
- <country>Norway\
-</country>\x0a    <\
-/homeAddress>\x0a</\
-contact>\x0a\
-\x00\x00\x03g\
-<\
-?xml version=\x221.\
-0\x22?>\x0a<xsd:schema\
- xmlns:xsd=\x22http\
-://www.w3.org/20\
-01/XMLSchema\x22>\x0a\x0a\
-    <xsd:element\
- name=\x22order\x22>\x0a \
-       <xsd:comp\
-lexType>\x0a       \
-     <xsd:sequen\
-ce>\x0a            \
-    <xsd:element\
- name=\x22customerI\
-d\x22 type=\x22xsd:pos\
-itiveInteger\x22/>\x0a\
-                \
-<xsd:element nam\
-e=\x22article\x22 type\
-=\x22articleType\x22 m\
-axOccurs=\x22unboun\
-ded\x22/>\x0a         \
-       <xsd:elem\
-ent name=\x22delive\
-ryDate\x22 type=\x22xs\
-d:date\x22/>\x0a      \
-          <xsd:e\
-lement name=\x22pay\
-ed\x22 type=\x22xsd:bo\
-olean\x22/>\x0a       \
-     </xsd:seque\
-nce>\x0a        </x\
-sd:complexType>\x0a\
-    </xsd:elemen\
-t>\x0a\x0a    <xsd:com\
-plexType name=\x22a\
-rticleType\x22>\x0a   \
-     <xsd:sequen\
-ce>\x0a            \
+name=\x22payed\x22 typ\
+e=\x22xsd:boolean\x22/\
+>\x0d\x0a            <\
+/xsd:sequence>\x0d\x0a\
+        </xsd:co\
+mplexType>\x0d\x0a    \
+</xsd:element>\x0d\x0a\
+\x0d\x0a    <xsd:compl\
+exType name=\x22art\
+icleType\x22>\x0d\x0a    \
+    <xsd:sequenc\
+e>\x0d\x0a            \
 <xsd:element nam\
 e=\x22articleId\x22 ty\
 pe=\x22xsd:positive\
-Integer\x22/>\x0a     \
-       <xsd:elem\
-ent name=\x22count\x22\
- type=\x22xsd:posit\
-iveInteger\x22/>\x0a  \
-          <xsd:e\
-lement name=\x22com\
-ment\x22 type=\x22xsd:\
-string\x22 minOccur\
-s=\x220\x22/>\x0a        \
-</xsd:sequence>\x0a\
-    </xsd:comple\
-xType>\x0a\x0a</xsd:sc\
-hema>\x0a\
-\x00\x00\x01\xb6\
+Integer\x22/>\x0d\x0a    \
+        <xsd:ele\
+ment name=\x22count\
+\x22 type=\x22xsd:posi\
+tiveInteger\x22/>\x0d\x0a\
+            <xsd\
+:element name=\x22c\
+omment\x22 type=\x22xs\
+d:string\x22 minOcc\
+urs=\x220\x22/>\x0d\x0a     \
+   </xsd:sequenc\
+e>\x0d\x0a    </xsd:co\
+mplexType>\x0d\x0a\x0d\x0a</\
+xsd:schema>\x0d\x0a\
+\x00\x00\x01\xc8\
 <\
-order>\x0a    <cust\
-omerId>194223</c\
-ustomerId>\x0a    <\
-article>\x0a       \
- <articleId>2224\
-2</articleId>\x0a  \
-      <count>5</\
-count>\x0a    </art\
-icle>\x0a    <artic\
-le>\x0a        <art\
-icleId>32372</ar\
-ticleId>\x0a       \
- <count>12</coun\
-t>\x0a        <comm\
-ent>without stri\
-pes</comment>\x0a  \
-  </article>\x0a   \
- <article>\x0a     \
-   <articleId>23\
-649</articleId>\x0a\
-        <count>2\
-</count>\x0a    </a\
-rticle>\x0a    <del\
-iveryDate>2009-0\
-1-23</deliveryDa\
-te>\x0a    <payed>t\
-rue</payed>\x0a</or\
-der>\x0a\
+order>\x0d\x0a    <cus\
+tomerId>194223</\
+customerId>\x0d\x0a   \
+ <article>\x0d\x0a    \
+    <articleId>2\
+2242</articleId>\
+\x0d\x0a        <count\
+>5</count>\x0d\x0a    \
+</article>\x0d\x0a    \
+<article>\x0d\x0a     \
+   <articleId>32\
+372</articleId>\x0d\
+\x0a        <count>\
+12</count>\x0d\x0a    \
+    <comment>wit\
+hout stripes</co\
+mment>\x0d\x0a    </ar\
+ticle>\x0d\x0a    <art\
+icle>\x0d\x0a        <\
+articleId>23649<\
+/articleId>\x0d\x0a   \
+     <count>2</c\
+ount>\x0d\x0a    </art\
+icle>\x0d\x0a    <deli\
+veryDate>2009-01\
+-23</deliveryDat\
+e>\x0d\x0a    <payed>t\
+rue</payed>\x0d\x0a</o\
+rder>\x0d\x0a\
 "
 
 qt_resource_name = b"\
@@ -365,24 +331,24 @@ qt_resource_name = b"\
 qt_resource_struct = b"\
 \x00\x00\x00\x00\x00\x02\x00\x00\x00\x09\x00\x00\x00\x01\
 \x00\x00\x00\x00\x00\x00\x00\x00\
-\x00\x00\x00^\x00\x00\x00\x00\x00\x01\x00\x00\x07\x9d\
-\x00\x00\x01e\xaf\x16\xd2\xa1\
-\x00\x00\x01\x04\x00\x00\x00\x00\x00\x01\x00\x00\x10\xb2\
-\x00\x00\x01e\xaf\x16\xd2\xa1\
-\x00\x00\x00\x1e\x00\x00\x00\x00\x00\x01\x00\x00\x01\x85\
-\x00\x00\x01e\xaf\x16\xd2\xa1\
-\x00\x00\x00\xa2\x00\x00\x00\x00\x00\x01\x00\x00\x0a\xe7\
-\x00\x00\x01e\xaf\x16\xd2\xa1\
-\x00\x00\x00\xc4\x00\x00\x00\x00\x00\x01\x00\x00\x0c\x19\
-\x00\x00\x01e\xaf\x16\xd2\xa1\
-\x00\x00\x00\x80\x00\x00\x00\x00\x00\x01\x00\x00\x09\xc6\
-\x00\x00\x01e\xaf\x16\xd2\xa1\
-\x00\x00\x00@\x00\x00\x00\x00\x00\x01\x00\x00\x03\xde\
-\x00\x00\x01e\xaf\x16\xd2\xa1\
+\x00\x00\x00^\x00\x00\x00\x00\x00\x01\x00\x00\x05\x17\
+\x00\x00\x01rR\x22\xae\x00\
+\x00\x00\x01\x04\x00\x00\x00\x00\x00\x01\x00\x00\x0es\
+\x00\x00\x01rR\x22\xae\x00\
+\x00\x00\x00\x1e\x00\x00\x00\x00\x00\x01\x00\x00\x01\x88\
+\x00\x00\x01rR\x22\xae\x00\
+\x00\x00\x00\xa2\x00\x00\x00\x00\x00\x01\x00\x00\x08y\
+\x00\x00\x01rR\x22\xae\x00\
+\x00\x00\x00\xc4\x00\x00\x00\x00\x00\x01\x00\x00\x09\xb8\
+\x00\x00\x01rR\x22\xae\x00\
+\x00\x00\x00\x80\x00\x00\x00\x00\x00\x01\x00\x00\x07M\
+\x00\x00\x01rR\x22\xae\x00\
+\x00\x00\x00@\x00\x01\x00\x00\x00\x01\x00\x00\x03\xef\
+\x00\x00\x01rR\x22\xae\x00\
 \x00\x00\x00\x00\x00\x01\x00\x00\x00\x01\x00\x00\x00\x00\
-\x00\x00\x01e\xaf\x16\xd2\xa1\
-\x00\x00\x00\xe6\x00\x00\x00\x00\x00\x01\x00\x00\x0dG\
-\x00\x00\x01e\xaf\x16\xd2\xa1\
+\x00\x00\x01rR\x22\xae\x00\
+\x00\x00\x00\xe6\x00\x00\x00\x00\x00\x01\x00\x00\x0a\xf1\
+\x00\x00\x01rR\x22\xae\x00\
 "
 
 def qInitResources():
